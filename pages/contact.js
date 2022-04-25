b39e502446c61661405b4f948d7dd72a1d692e77
Rounak Soni <r.soni@prefme.com>
2022-04-25 15:50:06 +0530

    inital commit

--- pages/contact.js
+++ pages/contact.js
@@ -2,44 +2,23 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
 import Link from 'next/link'
-
-
-
-
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <meta name ="keywords" content ="exact loaction, Pin, Map" />
-        <meta name="description" content="Easy way to explainyour address" />
+      <title>About Us</title>
+    <meta name ="keywords" content =" Digital Addressing, Game Changer" />
+    <meta name="description" content="The new symbol ^ of address" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to 
+          Welcome to Contact US
         </h1>
-        <ul className={styles.list}>
-  <li>
-  <Link href="/">
-  <a>Home</a>
-  </Link>
-  </li>
-  <li>
-  <Link href="/about">
-  <a >About Us</a>
-  </Link>
-  </li>
-  <li>
-  <Link href="/contact">
-  <a >Contact Us</a>
-  </Link>
-  </li>
-  
-</ul>
 
-      
+       
+       
       </main>
 
       <footer className={styles.footer}>
